f27b5084f02b2ad6f3a65c6b6f5608273cc3e8bd
0102johnny <rt2824.recruit.1022@gmail.com>
2024-01-05 16:27:55 +0900

    [add] Cellに表示させるドメインデータを要素とした配列を追加

--- SampleCompositionalLayouts/ViewController.swift
+++ SampleCompositionalLayouts/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 final class ViewController: UIViewController {
     @IBOutlet private weak var collectionView: UICollectionView!
 
+    /// Cellに表示させるドメインデータを要素とした配列
+    private let menuList = ["メニュー1","メニュー2","メニュー3","メニュー4","メニュー5","メニュー6",]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
