81d54a51085ebd1f065ea74db7f155a07cde75d3
0102johnny <rt2824.recruit.1022@gmail.com>
2024-01-05 15:59:34 +0900

    [add] 画面にCollectionViewを追加

--- SampleCompositionalLayouts/ViewController.swift
+++ SampleCompositionalLayouts/ViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
