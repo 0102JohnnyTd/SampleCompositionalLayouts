eeed4f6acc764ff7797a6e32a713d7c32e673d33
0102johnny <rt2824.recruit.1022@gmail.com>
2024-01-05 16:29:50 +0900

    [add] DiffableDataSourceに追加するItemとSectionを管理するenumを作成

--- SampleCompositionalLayouts/ViewController.swift
+++ SampleCompositionalLayouts/ViewController.swift
@@ -13,6 +13,27 @@ final class ViewController: UIViewController {
     /// Cellに表示させるドメインデータを要素とした配列
     private let menuList = ["メニュー1","メニュー2","メニュー3","メニュー4","メニュー5","メニュー6",]
 
+    /// DiffableDataSourceに追加するItemを管理
+    private enum Item: Hashable {
+        case todo(String)
+    }
+
+    // DiffableDataSourceに追加するSectionを管理
+    private enum Section: Int, CaseIterable {
+        case todoList
+        case timeLine
+
+        // Sectionごとの列数を返す
+        var columnCount: Int {
+            switch self {
+            case .todoList:
+                return 1
+            case .timeLine:
+                return 1
+            }
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
